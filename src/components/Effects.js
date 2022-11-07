acf5420e16477d04c1dff9a8667647f82aa3fb6c
Mufeed k <mufeedmangalad@gmail.com>
2022-11-07 19:26:07 +0530

    First Commit

---
+++ src/components/Effects.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
+
+const Effects = () => {
+    const [posts, setPosts] = useState([])
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(posts => setPosts(posts.splice(0, 10)))
+    }, [])
+    return (
+        <div>
+            <h1 className='bg-info text-white'>Fetch API</h1>
+            {posts.map((data) =>
+                <div key={data.id}>
+                    <h1 className='bg-dark text-white'>{data.title}</h1>
+                    <p className='text-danger'>{data.body}</p>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Effects
